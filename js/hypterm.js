e02f6f31ffe7e92b2c25ed1c35ac5be26aebc3b5
Jack Nullbyte <jackmeep123@gmail.com>
2018-02-24 12:52:50 +0100

    add scroll

--- js/hypterm.js
+++ js/hypterm.js
@@ -3,14 +3,15 @@ var commandwarg = null;
 $(document).keyup(function(event) {
     if ($('#cmdline').is(":focus") && event.key == "Enter" && $('#cmdline').val() != "" && $('#cmdline').val() != null) {
     	commandwarg = $('#cmdline').val();
-    	console.log($('#cmdline').val())
+    	console.log($('#cmdline').val());
         $('#outputterm').append($('#cmdline').val() + "\n");
-        var cmdtbl = commandwarg.split("@")
-        var cmd = cmdtbl[0]
-        var cmdargs = cmdtbl
-		cmdargs.splice(0,1)
-		var cmdargs2 = cmdargs.join("@")
-		var args = cmdargs2.split("@")
+        $('#outputterm').scrollTop($('#outputterm')[0].scrollHeight);
+        var cmdtbl = commandwarg.split("@");
+        var cmd = cmdtbl[0];
+        var cmdargs = cmdtbl;
+		cmdargs.splice(0,1);
+		var cmdargs2 = cmdargs.join("@");
+		var args = cmdargs2.split("@");
         if(cmd == "login" && args == "706986372" && loggedin == false){
         	loggedin = true;
         	$('#outputterm').append("Welcome, CEO! You are now logged in!" + "\n");
