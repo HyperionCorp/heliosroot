6890637de6a0866c0c9b7a9dbb16eb9df1daff94
Jack Nullbyte <jackmeep123@gmail.com>
2018-02-27 09:59:56 +0100

    fix some stuff, add some stuff

--- js/start.js
+++ js/start.js
@@ -1,4 +1,24 @@
- $(".asdf").sideNav();
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+$(".asdf").sideNav();
+
+var options = [
+{selector: '#hel', offset: 200, callback: function() {
+  Materialize.showStaggeredList('#hel');
+} },
+{selector: '#lb', offset: 200, callback: function() {
+  Materialize.showStaggeredList('#lb');
+} },
+{selector: '#wp', offset: 200, callback: function() {
+  Materialize.showStaggeredList('#wp');
+} },
+{selector: '#sh', offset: 200, callback: function() {
+  Materialize.showStaggeredList('#sh');
+} },
+];
+Materialize.scrollFire(options);
+
  $(document).ready(function(){
   $('.tap-target').tapTarget('open');
  });
